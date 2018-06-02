7a8b16e0f8d3a2b640e82c1d67da39315af4ae84
Jeremy Peterson <jeremy.one@gmail.com>
2018-06-01 23:30:56 -0700

    First!

---
+++ HashExtentions.cs
@@ -0,0 +1,20 @@
+﻿using CryptLink.SigningFramework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CryptLink.ConsistentHash
+{
+    public static class HashExtentions {
+
+        public static Hash Rehash(this Hash thisHash) {
+
+            if (thisHash.Bytes == null) {
+                throw new NullReferenceException("Byte array on hash was null, can't rehash");
+            }
+
+            return Hash.Compute(thisHash.Bytes, thisHash.Provider.Value, default(Cert));
+        }
+
+    }
+}
